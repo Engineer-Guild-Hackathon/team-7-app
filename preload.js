a6871c9f483df28467dba6b66f62b6bb3948ecc2
marudemaru <marudemaruyuki@gmail.com>
2025-09-11 16:07:09 +0900

    chore: フロントエンド・不要な設定ファイルやディレクトリを整理・削除

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  getUsage: () => ipcRenderer.invoke('get-usage'),
-  updateCategory: (appId, newType) =>
-    ipcRenderer.invoke('update-category', { appId, newType }),
-});
